ed43a62b7981f1eb750665252f6002e7dd4cfe60
cadeteortiz0-sudo <cadeteortiz0@gmail.com>
2026-07-12 19:28:49 -0500

    añadi aprendizages en clases

--- 01_Fundamentos/main.py
+++ 01_Fundamentos/main.py
@@ -2,3 +2,5 @@ print('hello world')
 print('agregue mi primer cambio')
 
 print('agregue mi segundo cambio')
+print('f'  ')
+#iva vale = 0.15
---
+++ iva/iva.py
@@ -0,0 +1,8 @@
+#Costo total mas el iva  
+precio = float(input('Iingrese el precio'))
+ 
+iva = precio * 0.15 
+iva = precio + iva 
+
+print('iva:', iva)
+print ('total:', total)
